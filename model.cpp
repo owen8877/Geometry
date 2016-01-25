68809354a494769b5954805d9fc81e706317adab
shsty <shsty97@163.com>
2016-01-25 15:26:53 +0800

    Added track, smooth motion.

--- model.cpp
+++ model.cpp
@@ -20,9 +20,10 @@ vector<point> v;
 vector<line> l;
 vector<int> vr, vg, vb;
 vector<int> lr, lg, lb;
-transform t(0.0);
+transform t(0.0), t1(0.0);
 
 deque<point> snake;
+size_t snakeend;
 point food;
 double snakeStep = 0.2;
 double boundRadius = 2;
@@ -31,8 +32,10 @@ int snakeDir = 0;
 int snakeInc;
 int score = 0;
 
+int stepInterval = 5;
+
 const ideal dirs[4] = {ideal(0.0, 1.0), ideal(-1.0, 0.0), ideal(0.0, -1.0), ideal(1.0, 0.0)};
-point steps[4];
+point steps[8];
 
 void new_point_in_v(){
     double ran = (double) rand()/RAND_MAX*2 - 1;
@@ -58,9 +61,11 @@ void add_line(line _l){
 
 void initModel(){
     for (int i = 0; i < 4; ++i) steps[i] = getPointByDistance(point(0.0), dirs[i], snakeStep);
+    for (int i = 4; i < 8; ++i) steps[i] = getPointByDistance(point(0.0), dirs[i-4], snakeStep/stepInterval);
     snake.clear();
     snake.push_back(point(0.0, 0.0));
-    t = transform(0.0);
+    snakeend = 0;
+    t = t1 = transform(0.0);
     northDir = ideal(1.0, 0.0);
     snakeDir = 0;
     snakeInc = 10;
@@ -73,7 +78,7 @@ bool checkCollision(){
         printf("You reached the boundary.\n");
         return true;
     }
-    for (deque<point>::iterator it = ++snake.begin(); it != snake.end(); ++it){
+    for (deque<point>::reverse_iterator it = snake.rbegin() + snakeend; it != snake.rend()-1; ++it){
         if (PoincareDistance(*snake.begin(), *it) >= snakeStep*.99) continue;
         printf("You ate yourself.\n");
         return true;
@@ -85,9 +90,13 @@ bool checkFood(){
     return (PoincareDistance(food, *snake.begin()) <= snakeStep);
 }
 
+point randFoodPoint(){
+    static double C = cosh(boundRadius) - 1;
+    return point(tanh(acosh(C*rand()/RAND_MAX + 1)/2) , 0.0).rotate(2 * M_PI * rand()/RAND_MAX);
+}
 
 void update(int kbstat[]){
-    static int stepInterval = 5 ,frameCount = 1;
+    static int frameCount = stepInterval;
     static int snakeDirNext = 0;
     if (kbstat['w']) snakeDirNext = 0; 
     if (kbstat['a']) snakeDirNext = 1; 
@@ -97,26 +106,29 @@ void update(int kbstat[]){
         
         if ((snakeDirNext + snakeDir)%2) snakeDir = snakeDirNext;
         snake.push_front(t.inversion()(steps[snakeDir]));
-        t = transform(steps[snakeDir]) * t;
+        t1 = transform(steps[snakeDir]) * t1;
+        t = transform(steps[snakeDir+4]) * t;
 
         if (snakeInc > 0) snakeInc--;
-        else snake.pop_back();
+        else snakeend++;
 
         if (checkCollision()){
             printf("You lose! Score: %d\n\n", score);
             fflush(stdout);
             initModel();
-            frameCount = 0;
+            frameCount = stepInterval;
+            snakeDir = 0;
             snakeDirNext = 0;
+            return;
         }
         
         if (checkFood()){
             snakeInc += 2;
             score++;
-            while(PoincareDistance(complex(0.0), (food = randPoint())) >= boundRadius);
+            while(PoincareDistance(complex(0.0), (food = randFoodPoint())) >= boundRadius);
         }
         frameCount = 0;
-    }
+    } else t = transform(steps[snakeDir+4]) * t;
     ++frameCount;
 }
 
